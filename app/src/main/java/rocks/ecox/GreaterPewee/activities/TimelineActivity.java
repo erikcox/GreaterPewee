ba79a99c9730fb8c9cbd13d0edcbfead2bf2c23e
Erik Cox <erikbcox@gmail.com>
2016-11-14 20:40:26 -0800

    feat: Add PagerSlidingTabStrip

--- app/src/main/java/rocks/ecox/GreaterPewee/activities/TimelineActivity.java
+++ app/src/main/java/rocks/ecox/GreaterPewee/activities/TimelineActivity.java
@@ -1,9 +1,17 @@
 package rocks.ecox.GreaterPewee.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.astuetz.PagerSlidingTabStrip;
+
 import rocks.ecox.GreaterPewee.R;
+import rocks.ecox.GreaterPewee.fragments.HomeTimelineFragment;
+import rocks.ecox.GreaterPewee.fragments.MentionsTimelineFragment;
 import rocks.ecox.GreaterPewee.fragments.TweetsListFragment;
 
 public class TimelineActivity extends AppCompatActivity {
@@ -15,5 +23,48 @@ public class TimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+        // Get the ViewPager
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+        // Set the ViewPager adapter for the pager
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager()));
+        // Find the pager sliding tabstrip
+        PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        // Attach the pager tabstrip to the ViewPager
+        tabStrip.setViewPager(vpPager);
+    }
+
+    // Return the order of the Fragments in the ViewPager
+    public class TweetsPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 2; // Timeline and Mentions
+        private String tabTitles[] = {"Home", "Mentions"};
+
+        // Adapter gets the manager insert or remove Fragment from Activity
+        public TweetsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        // The order and creation of Fragments within the pager
+        @Override
+        public Fragment getItem(int position) {
+            if(position == 0) {
+                return new HomeTimelineFragment();
+            } else if(position == 1) {
+                return new MentionsTimelineFragment();
+            } else {
+                return null;
+            }
+        }
+
+        // Return the tab title
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        // How many Fragments there are to swipe between
+        @Override
+        public int getCount() {
+             return tabTitles.length;
+        }
     }
 }
